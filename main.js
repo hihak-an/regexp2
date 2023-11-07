05d9fec596e312e35303b48b17e1ec134af7b38d
hihak-an <ayh525@naver.com>
2023-11-07 19:52:07 +0900

    clean push

---
+++ main.js
@@ -0,0 +1,25 @@
+let str = `
+010-1234-5678.
+thehihak@gamil.com
+https://omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+d`
+
+
+console.log(
+    str.match(/http/g)
+    )
+
+
+
+// console.log(
+//     str.match(/\.$/gim)
+//     )
+
+
+// str = str.replace(regexp, 'AAA')
+// console.log(str)
+
+// console.log(str.match(regexp))
+// console.log(regexp.test(str))
